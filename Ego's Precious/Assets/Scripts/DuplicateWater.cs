952395062f0f795f4f1599d24b26e9f24d0ea2e7
Rune <rune@vanderlei.be>
2023-02-15 11:05:17 +0100

    water duplicator

--- Ego's Precious/Assets/Scripts/DuplicateWater.cs
+++ Ego's Precious/Assets/Scripts/DuplicateWater.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class DuplicateWater : MonoBehaviour
 {
+
+    public GameObject waterPlane;
+
+    public Vector3 waterPlaneStartPos;
+    public float waterPlaneSize;
+    public int colCount;
+    public int rowCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int x = 0; x < colCount; x++)
+        {
+            for (int z = 0; z < rowCount; z++)
+            {
+                Instantiate(waterPlane, new Vector3(waterPlaneStartPos.x + (x * waterPlaneSize), waterPlaneStartPos.y, waterPlaneStartPos.z + (z * waterPlaneSize)), Quaternion.identity);
+            }
+        }
     }
 }
