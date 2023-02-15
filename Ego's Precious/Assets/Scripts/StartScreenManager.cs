69d0b56b1164559e9a80dd28c0b10f35d3380f38
Rune <rune@vanderlei.be>
2023-02-15 21:28:28 +0100

    Start Screen

---
+++ Ego's Precious/Assets/Scripts/StartScreenManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class StartScreenManager : MonoBehaviour
+{
+    [SerializeField] private GameObject startScreen;
+    
+    public void OnStartGame(InputAction.CallbackContext context)
+    {
+        Debug.Log("Start Game");
+        //startScreen.SetActive(false);
+        SceneManager.LoadScene("Game");
+        
+    }
+}
