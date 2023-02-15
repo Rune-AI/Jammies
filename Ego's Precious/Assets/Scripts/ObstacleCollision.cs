b8533a24c3fcddb8d36fae777a9402b1b2690338
Rune <rune@vanderlei.be>
2023-02-15 01:39:21 +0100

    added sticker collision and new asset package

--- Ego's Precious/Assets/Scripts/ObstacleCollision.cs
+++ Ego's Precious/Assets/Scripts/ObstacleCollision.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObstacleCollision : MonoBehaviour
 {
     public GameObject HolePrefab;
-    
+    public GameObject StickerPrefab;
+
 
     private GameObject HolesParent;
+    private GameObject StickersParent;
 
     private void Awake()
     {
@@ -15,14 +18,82 @@ public class ObstacleCollision : MonoBehaviour
         if (HolesParent == null)
         {
             HolesParent = new GameObject("Holes");
+            HolesParent.transform.parent = this.transform;
+        }
+
+
+        StickersParent = GameObject.Find("Stickers");
+        if (StickersParent == null)
+        {
+            StickersParent = new GameObject("Stickers");
+            StickersParent.transform.parent = this.transform;
         }
     }
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision with " + collision.gameObject.name);
 
-        Quaternion rotation = Quaternion.LookRotation(collision.contacts[0].normal);
-        Vector3 position = collision.contacts[0].point;
-        GameObject hole = Instantiate(HolePrefab, position, rotation, HolesParent.transform);
+        switch (collision.gameObject.tag)
+        {
+            case "Obstacle":
+                addPrefab(HolePrefab, collision.contacts[0].point, collision.contacts[0].normal, HolesParent);
+                break;
+            case "Sticker":
+                addPrefab(StickerPrefab, collision.contacts[0].point, collision.contacts[0].normal, StickersParent);
+                Destroy(collision.gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger with " + other.name);
+
+        Vector3 direction =  transform.position - other.transform.position;
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(other.transform.position, direction, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+
+
+        if (!hit.transform.gameObject.CompareTag("Boat"))
+        {
+            Debug.Log("Not A boat");
+            return;
+        }
+
+        Vector3 point = hit.point;
+        Vector3 normal = -hit.normal;
+        
+        switch (other.gameObject.tag)
+        {
+            case "Obstacle":
+                addPrefab(HolePrefab, point, normal, HolesParent);
+                break;
+            case "Sticker":
+                addPrefab(StickerPrefab, point, normal, StickersParent);
+                Destroy(other.gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void addPrefab(GameObject prefab, Vector3 point, Vector3 normal, GameObject parent)
+    {
+        Quaternion rotation = Quaternion.LookRotation(normal);
+        Vector3 position = point;
+        GameObject hole = Instantiate(prefab, position, rotation, parent.transform);
     }
 }
